65002314fd7c9a3bef43b1ecc96f80a328c92e44
Ryan <ryan@rnl.life>
2016-03-17 20:35:45 -0400

    import

---
+++ js/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ryanApp
+ * @description
+ * # ryanApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ryanApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ js/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ryanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ryanApp
+ */
+angular.module('ryanApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
